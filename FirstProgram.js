2bbd3e6a1015a6ee00e2928f2985a2d665d23423
Hanan9393 <163463769+Hanan9393@users.noreply.github.com>
2024-05-12 11:57:42 -0700

    Add files via upload

---
+++ FirstProgram.js
@@ -0,0 +1,7 @@
+console.log("before function");
+function sum(num1, num2) {
+    return num1 + num2;
+}
+sum(4, 5);
+console.log(sum(5, 7));
+console.log("after function");
---
+++ FirstProgram.ts
@@ -0,0 +1,8 @@
+console.log("before function")
+function sum(num1:number,num2:number){
+    return num1+num2;
+}
+sum(4,5);
+console.log(sum(5,7));
+console.log("after function")
+
