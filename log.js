bad1f77a2f4cf81e465c6b3a6788d3b920ca6916
Hanan9393 <163463769+Hanan9393@users.noreply.github.com>
2024-06-01 03:53:06 -0700

    Add files via upload
    logical operators

---
+++ log.js
@@ -0,0 +1,8 @@
+var a = 5;
+var b = 2;
+var logicalAnd = (a > 0) && (b > 0); // true
+var logicalOr = (a < 0) || (b > 0); //true
+var logicalNot = !(a > 0); // false
+console.log(logicalAnd);
+console.log(logicalOr);
+console.log(logicalNot);
---
+++ log.ts
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 2;
+let logicalAnd = (a>0)&&(b >0); // true
+let logicalOr = (a < 0)|| (b > 0); //true
+let logicalNot = !(a>0); // false
+
+console.log(logicalAnd)
+console.log(logicalOr)
+console.log(logicalNot)
